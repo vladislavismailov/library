105fc423dd90693ebbaccc36b56ee15b4d86815c
Vladislav Ismailov <vladislavismailov55555@gmail.com>
2025-11-07 21:33:43 +0500

    День 7. Реализован возврат книги по ИИН

--- return_book.py
+++ return_book.py
@@ -42,6 +42,35 @@ def show_menu(menu):
 
 
         if num == 2:
-            print("Эта функция будет реализована позже: ")
+            iin = input("Введите ИИН Пользователя Который Возвращает Книгу: ")
+            user_books = []
+            name = ""
+            for book in books:
+                for user in book["users"]:
+                    if user["iin"] == iin:
+                        user_books.append(book)
+                        if len(name) == 0:
+                            name = user["fio"]
+                    
+            print(f"На ИИН {iin, name} Найдены Следующие Книги:")
+            index = 1
+            for book in user_books:
+                print(f"{index} - ({book["isbn"]}) {book["title"]}")
+                index += 1
+            while True:
+                index = int(input("Введите номер книги: "))
+                if index >= 1 and index <= len(user_books):
+                    break
+                print(f'{Fore.RED}Ошибка.  Введён неверный номер.{Style.RESET_ALL}')
+
+            index -= 1
+            user_books[index]["amount"] += 1
+            
+            for idx, user in enumerate(user_books[index]["users"]):
+                    if user["iin"] == iin:
+                        user_books[index]["users_history"].append(user_books[index]["users"].pop(idx))
+                        db.db_write(books)
+                        print(f"{Fore.GREEN}Книжка успешно возвращена{Style.RESET_ALL}")
+                        break
         if num == 0:
             break
